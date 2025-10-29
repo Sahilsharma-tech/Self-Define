4296d51e39f58222e37d0ff2e19ef5270fed8f52
Sahilsharma-tech <sahil.sharmaedu25@gmail.com>
2025-10-29 22:59:59 +0530

    Update hello_world.cpp

--- Class 1 (Introduction)/hello_world.cpp
+++ Class 1 (Introduction)/hello_world.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std; // We have used this to prevent writing std:: every time
 
+// Function Decleration starts
+void function() { // Void means that no value is returned from this function.
+    cout << "Hello World"; // Prints Hello World
+}
+// Function Decleration ends
+
+
+int main() { // Main function (Program execution starts here)
+    printHelloWorld(); // Function is being called
+    return 0; // Indicates that the program runs successfully
+}
