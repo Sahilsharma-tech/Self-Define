48e43606da30403ac32d1dc417994b484a5e6613
Sahilsharma-tech <sahil.sharmaedu25@gmail.com>
2025-10-30 22:39:11 +0530

    Update number_pattern.cpp

--- Class 2 (Syntax)/number_pattern.cpp
+++ Class 2 (Syntax)/number_pattern.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for(int i = 0; i <= n; i++){
-        for(int j = 0; j < i; j++){
+    for(int i = 0; i <= n; i++){ // this controls the number of rows
+        for(int j = 0; j < i; j++){ // this controls the number of times to be printed in the row
             cout << i;
         }
-    cout << endl;
+    cout << endl; // moves to new line
     }
 }
