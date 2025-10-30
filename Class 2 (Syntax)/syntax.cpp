ac756598941842484e2e1af0d1bdb0fb357d6422
Sahilsharma-tech <sahil.sharmaedu25@gmail.com>
2025-10-30 21:18:19 +0530

    Create syntax.cpp

---
+++ Class 2 (Syntax)/syntax.cpp
@@ -0,0 +1,8 @@
+// #include <iostream> // used to import cin and cout functions in the program.
+#include <bits/stdc++.h> // This includes all the libraries.
+int main () {
+  int a;
+  std::cin >> a;
+  std::cout << a << std::endl; // We use endl to move the cursor to the next line. \n can also be used.
+  return 0;
+}
