a8bd6670ef22812f394c51c97d926cdaa0eab532
Sahilsharma-tech <sahil.sharmaedu25@gmail.com>
2025-10-30 21:38:48 +0530

    Update Fuzzbizz.cpp

--- Class 2 (Syntax)/Fuzzbizz.cpp
+++ Class 2 (Syntax)/Fuzzbizz.cpp
@@ -2,18 +2,17 @@
 using namespace std;
 int main () {
     int n;
-    cin >> n;
-    
-    if(n % 3 == 0 and n % 5 == 0){
+    cin >> n;    
+    if(n % 3 == 0 and n % 5 == 0){ // Check for divisible by 3 and 5
         cout << "Fizzbuzz" << endl;
     }
-    else if(n % 3 == 0){
+    else if(n % 3 == 0){ // Check for divisible by 3
         cout << "Fizz" << endl;
     }
-    else if(n % 5 == 0){
+    else if(n % 5 == 0){ // Check for divisible by 5
         cout << "Buzz" << endl;
     }
-    else{
+    else{ // Print the number itself if not divisible by 3 or 5
         cout << n << endl;
     }
 }
