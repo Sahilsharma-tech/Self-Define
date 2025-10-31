261c7311b94ab40ed5002abcac1937a567ad381e
Sahilsharma-tech <sahil.sharmaedu25@gmail.com>
2025-11-01 00:34:23 +0530

    Update reverse_swap.cpp

--- Class 3 (arrays)/reverse_swap.cpp
+++ Class 3 (arrays)/reverse_swap.cpp
@@ -16,7 +16,17 @@ int main() {
     
     for(int i = 0; i < n; i++) {
         cout << nums[i] << " ";
-    }    
-
-    
+    }        
 }
+
+/*
+Here is the working to how the swapping will work.
+Lets say array is 1 2 3 4 5,
+after first step => 5 2 3 4 1 
+after second step -> 5 4 3 2 1
+for this case, n/2 = 5/2 = 2.5 which will be taken as 2 since int/int always gives int.
+Now, if you run this till n, instead of n/2, 
+third step -> 5 2 3 4 1 
+fourth step -> 1 2 3 4 5
+This will return the array to its original position, hence, always remember to run till HALF.
+*/
