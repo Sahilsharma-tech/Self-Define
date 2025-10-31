2cd51fce63ca08cbfb46cae48451baaffcfe1056
Sahilsharma-tech <sahil.sharmaedu25@gmail.com>
2025-11-01 00:28:49 +0530

    Update sorted.cpp

--- Class 3 (arrays)/sorted.cpp
+++ Class 3 (arrays)/sorted.cpp
@@ -10,8 +10,8 @@ int main () {
     }
     
     bool sorted = true;
-    for(int i = 1; i < n; i++){
-        if(nums[i-1] > nums[i]){
+    for(int i = 1; i < n; i++){ // we started this array from 1, to pervent going out of bounds while writing the next check
+        if(nums[i-1] > nums[i]){ // ye check ko dhyaan se dekho, i-1 and i ko compare krra hai. i-1 in this case will be 0 for first loop, hence this method works.
             sorted = false;
             break;
         }
@@ -23,4 +23,5 @@ int main () {
     else{
         cout << "Array is not sorted";
     }
+
 }
