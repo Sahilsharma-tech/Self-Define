612bf5747d117040bcbbccaf14dcb95b0c1a0fd3
Sahilsharma-tech <sahil.sharmaedu25@gmail.com>
2025-11-04 19:08:26 +0530

    Update max_profit.cpp

--- Class 4 (arrays 2)/Homework/max_profit.cpp
+++ Class 4 (arrays 2)/Homework/max_profit.cpp
@@ -1,1 +1,26 @@
-
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    cin >> n;
+    int nums[1000];
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 1; j < n; j++){
+            if(nums[j-1] > nums[j]){
+                swap(nums[j-1], nums[j]);
+            }
+        }
+    }
+    
+    int smallest = nums[0];
+    int largest = nums[n-1];
+    
+    int profit = largest - smallest;
+    cout << profit << endl;
+    
+    
+}
