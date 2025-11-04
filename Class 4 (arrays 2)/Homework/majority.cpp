81b49e7ca1aca56bb0839f9e5bb3b14cb6d1d819
Sahilsharma-tech <sahil.sharmaedu25@gmail.com>
2025-11-04 19:20:36 +0530

    Update majority.cpp

--- Class 4 (arrays 2)/Homework/majority.cpp
+++ Class 4 (arrays 2)/Homework/majority.cpp
@@ -1,1 +1,39 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int nums[1000];
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int majority = 0;  
+
+    for(int i = 0; i < n; i++) {
+        int count = 0;
+
+        for(int j = 0; j < n; j++) {
+            if(nums[i] == nums[j]) {
+                count++;
+            }
+        }
+
+        if(count > n / 2) {
+            majority = nums[i];
+            break;  
+        }
+    }
+
+    if(majority != 0){
+        cout << "majority element: " << majority;
+    }
+    else{
+        cout << "mo majority element";
+    }
+
+    return 0;
+}
