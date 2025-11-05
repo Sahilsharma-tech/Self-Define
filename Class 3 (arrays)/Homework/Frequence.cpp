33f9ccea94d4f3608086a03c3b29d714acea4dc3
Sahilsharma-tech <sahil.sharmaedu25@gmail.com>
2025-11-05 21:08:59 +0530

    Create Frequence.cpp

---
+++ Class 3 (arrays)/Homework/Frequence.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;          
+    int arr[1000];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    bool counted[n] = {false}; // Frequence Array approach hoti hai, very useful.
+
+    for (int i = 0; i < n; i++) {
+        if (counted[i]){
+        continue;
+        } 
+
+        int count = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                counted[j] = true;
+            }
+        }
+
+        cout << arr[i] << " appears " << count << " times\n";
+    }
+
+    return 0;
+}
